fbf9df9a6d2636b0b418f88e7e19377d9d04d88a
ehrgein <ehrgein@gmail.com>
2025-01-26 16:42:59 -0300

    lenis reset changes

--- src/app/helpers/ResetLenisLink.tsx
+++ src/app/helpers/ResetLenisLink.tsx
@@ -7,6 +7,9 @@ interface CustomLinkProps extends LinkProps {
   children: React.ReactNode;
 }
 
+// Smooth Scrolling causes issues with Dynamic Routes on Next JS. Issue here: https://github.com/vercel/next.js/discussions/64435
+// The solution is to disable the css for smooth scrolling, but Lenis has this without the css rule, so disabling Lenis works as well.
+// This Link component is required so that Lenis is restarted.
 export default function ResetLenisLink({
   children,
   className,
