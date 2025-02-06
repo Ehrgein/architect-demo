21a08d1f7397d8e5e38b5e5d1bdf631ca115f935
ehrgein <ehrgein@gmail.com>
2025-02-06 16:10:14 -0300

    test build

--- src/app/helpers/SmoothScrolling.tsx
+++ src/app/helpers/SmoothScrolling.tsx
@@ -1,30 +1,30 @@
 "use client";
-import React, { ReactNode } from "react";
-import { ReactLenis, useLenis } from "lenis/react";
-import { usePathname, useSearchParams } from "next/navigation";
+import React from "react";
+import { ReactLenis } from "lenis/react";
+import { usePathname } from "next/navigation";
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
-  const lenis = useLenis();
+  // const lenis = useLenis();
 
-  React.useEffect(() => {
-    //required to stop lenis when pathname changes as otherwise window y value is kept.
-    if (lenis) {
-      lenis.stop();
-    }
+  // React.useEffect(() => {
+  //   //required to stop lenis when pathname changes as otherwise window y value is kept.
+  //   if (lenis) {
+  //     lenis.stop();
+  //   }
 
-    // when visiting a new page, it will restart lenis and go to the top.
-    const handleScrollToTop = () => {
-      if (lenis) {
-        lenis.start();
-        window.scrollTo(0, 0);
-      }
-    };
+  //   // when visiting a new page, it will restart lenis and go to the top.
+  //   const handleScrollToTop = () => {
+  //     if (lenis) {
+  //       lenis.start();
+  //       window.scrollTo(0, 0);
+  //     }
+  //   };
 
-    handleScrollToTop();
-  }, [pathname, searchParams, lenis]);
+  //   handleScrollToTop();
+  // }, [pathname, searchParams, lenis]);
 
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.4 }}>
