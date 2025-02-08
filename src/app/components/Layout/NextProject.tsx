63494c297c99732c818eee25010a55f2fee89fd7
ehrgein <ehrgein@gmail.com>
2025-02-08 13:16:59 -0300

    Font fixes for small mobile devices when 1 word is too long.

--- src/app/components/Layout/NextProject.tsx
+++ src/app/components/Layout/NextProject.tsx
@@ -15,18 +15,18 @@ function NextProject({
   return (
     <>
       <section className="pt-36">
-        <div className="space-y-8">
+        <div className="mobilesm:space-y-3 md:space-y-8">
           <div className="w-fit pl-2">
             <h4 className="uppercase text-[#363636] tracking-wide py-1 text-sm ">
               Next Project
             </h4>
             <div className="bg-[#363636] h-[1px] w-full opacity-60"></div>
           </div>
-          <h2 className="uppercase leading-tight md:leading-none text-[4.5rem] md:text-7xl text-[#363636]">
+          <h2 className="uppercase leading-tight md:leading-none text-5xl mobilexl:text-[4.5rem] md:text-7xl text-[#363636]">
             {nextTitle}
           </h2>
         </div>
-        <div className="pt-10">
+        <div className="pt-4 md:pt-10">
           <TransitionLink href={`/projects/${nextSlug}`}>
             <ParallaxImage
               sizes="(max-width: 1920px) 95vw, 100vw"
